5aae67cc5613cdd39dab8de374f43377ed6191db
Kalin Chernev <kalin.chernev@gmail.com>
2019-04-13 14:36:37 +0300

    Settings files

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteTitle: 'Да спасим Кара дере', // Site title.
+  netlifyCmsHtmlTitle: 'Да спасим Кара дере | Администрация',
+  siteTitleAlt: 'Да спасим Кара дере', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://karadere.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription:
+    'Инициативата "Да спасим Кара дере" има за цел да съхрани един от последните незастроени райони по черноморския бряг, познат с неповторима красота и богата природа.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
+  disqusShortname: 'karadere', // Disqus shortname.
+  userName: 'website',
+  userTwitter: '',
+  userLocation: 'Bulgaria',
   userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  copyright: '', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
-}
+};
