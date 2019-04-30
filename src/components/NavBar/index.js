1072ba87c4e05edd0497ecf4321329e34a9bf035
Alexande Vasilev <alexander.vasilev@protonmail.com>
2019-04-30 13:02:23 +0300

    solving issue with StaticQuery from the old search plugin

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,59 +1,50 @@
 import React from 'react';
-import { Link, graphql, StaticQuery } from 'gatsby';
+import { Link } from 'gatsby';
 import SearchBox from '../SearchBox';
 import logo from './karadere_logo.png';
 
-const NavBar = ({ toggleNavbar, isActive }) => (
-  <StaticQuery
-    query={graphql`
-      query SearchIndexQuery {
-        siteSearchIndex {
-          index
-        }
-      }
-    `}
-    render={data => (
-      <nav className="navbar is-fixed-top" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item" title="Да спасим Кара дере">
-            <img typeof="foaf:Image" src={logo} alt="Лого на сайта" />
-          </Link>
-          <button
-            className={`button navbar-burger ${isActive ? 'is-active' : ''}`}
-            data-target="navMenu"
-            onClick={toggleNavbar}
-          >
-            <span />
-            <span />
-            <span />
-          </button>
-        </div>
-        <div
-          className={`navbar-menu ${isActive ? 'is-active' : ''}`}
-          id="navMenu"
+const NavBar = ({ toggleNavbar, isActive }) => {
+  return (
+    <nav className="navbar is-fixed-top" aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item" title="Да спасим Кара дере">
+          <img typeof="foaf:Image" src={logo} alt="Лого на сайта" />
+        </Link>
+        <button
+          className={`button navbar-burger ${isActive ? 'is-active' : ''}`}
+          data-target="navMenu"
+          onClick={toggleNavbar}
         >
-          <div className="navbar-end">
-            <Link className="navbar-item" to="/about">
-              За нас
-            </Link>
-            <Link className="navbar-item" to="/articles">
-              Актуално
-            </Link>
-            <div className="navbar-item">
-              <div className="field is-grouped">
-                <p className="control">
-                  <Link className="button is-primary is-outlined" to="/contact">
-                    Контакти
-                  </Link>
-                </p>
-              </div>
+          <span />
+          <span />
+          <span />
+        </button>
+      </div>
+      <div
+        className={`navbar-menu ${isActive ? 'is-active' : ''}`}
+        id="navMenu"
+      >
+        <div className="navbar-end">
+          <Link className="navbar-item" to="/about">
+            За нас
+          </Link>
+          <Link className="navbar-item" to="/articles">
+            Актуално
+          </Link>
+          <div className="navbar-item">
+            <div className="field is-grouped">
+              <p className="control">
+                <Link className="button is-primary is-outlined" to="/contact">
+                  Контакти
+                </Link>
+              </p>
             </div>
-            <SearchBox searchIndex={data.siteSearchIndex.index} />
           </div>
+          <SearchBox />
         </div>
-      </nav>
-    )}
-  />
-);
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
